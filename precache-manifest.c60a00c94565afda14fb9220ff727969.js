cb69bc661cf6190686d7fc83712fe92909f4e74e
tamara-f <filatovatam@ukr.net>
2020-09-09 00:02:40 +0300

    Updates

---
+++ precache-manifest.c60a00c94565afda14fb9220ff727969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b9f9b1df55a760f9713f8e4c75de11",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a98b6929169296f97fc7",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.8f756de5.chunk.css"
+  },
+  {
+    "revision": "b8635b865ccb11173ea2",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.24b3bf27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.24b3bf27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98b6929169296f97fc7",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.450bb708.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
